7d6e0c52a361eb92994b97b8e7e615827b4c7341
Ankit Singh <theankitsingh2001@gmail.com>
2021-10-29 11:40:55 +0530

    working on Help section

--- app/src/main/java/com/project/communicationhub/interaction/HelpActivity.kt
+++ app/src/main/java/com/project/communicationhub/interaction/HelpActivity.kt
@@ -1,12 +1,49 @@
 package com.project.communicationhub.interaction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.project.communicationhub.MainActivity
 import com.project.communicationhub.R
+import com.project.communicationhub.databinding.ActivityHelpBinding
 
 class HelpActivity : AppCompatActivity() {
+
+    // Initializing Variables
+    private lateinit var helpActivity: ActivityHelpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_help)
+        helpActivity = ActivityHelpBinding.inflate(layoutInflater)
+        setContentView(helpActivity.root)
+
+        setSupportActionBar(helpActivity.toolbarHelp)
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.setDisplayShowTitleEnabled(true)
+        }
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id: Int = item.itemId
+        if (id == R.id.home) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        finish()
+        startActivity(
+            Intent(this, MainActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
+    }
+
 }
